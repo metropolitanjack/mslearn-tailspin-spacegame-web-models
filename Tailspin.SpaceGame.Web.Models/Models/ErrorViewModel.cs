af80f91b973d1d4d483370f220da8f0b87b8162d
jack.ndupuechi <jack.ndupuechi@petroineos.com>
2019-10-03 09:21:15 +0100

    force rebuild

--- Tailspin.SpaceGame.Web.Models/Models/ErrorViewModel.cs
+++ Tailspin.SpaceGame.Web.Models/Models/ErrorViewModel.cs
@@ -3,7 +3,6 @@ namespace TailSpin.SpaceGame.Web.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
